2e24135628555187a10957d6bb9a1aaa5b7faf52
Carlos Moraes <113129034+Carlos-Moraes2@users.noreply.github.com>
2023-01-05 22:20:48 -0300

    criando as variáveis Js

--- imc.js
+++ imc.js
@@ -1,15 +1,15 @@
 const calcular = document.getElementById('calcular');
 
-
-
 function imc () {
     const nome = document.getElementById('nome').value;
     const altura = document.getElementById('altura').value;
     const peso = document.getElementById('peso').value;
     const resultado = document.getElementById('resultado');
 
-    if (nome !== '') {
-        alert ('valor preenchido!')
+    if (nome !== '' && altura !== '' && peso !== '') {
+        alert ('valor preenchido!');
+    }else{
+        resultado.textContent =  'Preencha todos os campos!!!'
     }
 }
 
