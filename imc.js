6391d0bb3bd00f9bd3be942cf4b443c48821d5ef
Carlos Moraes <113129034+Carlos-Moraes2@users.noreply.github.com>
2023-01-07 00:01:28 -0300

    adicionando condicionais if e else

--- imc.js
+++ imc.js
@@ -7,9 +7,29 @@ function imc () {
     const resultado = document.getElementById('resultado');
 
     if (nome !== '' && altura !== '' && peso !== '') {
-        alert ('valor preenchido!');
+
+        const valorIMC = (peso / (altura * altura)).toFixed(1);
+
+        let classificacao = '';
+
+        if (valorIMC < 18.5){
+            classificacao = 'abaixo do peso.';
+        }else if (valorIMC < 25) {
+            classificacao = 'com peso ideal. Parabéns!!!';
+        }else if (valorIMC <30){
+            classificacao = 'levemente acima do peso.';
+        }else if (valorIMC <35){
+            classificacao = 'com obesidade grau I.';
+        } else  if  ( valorIMC  <  40 ) {
+            classificação  =  'com obesidade grau II' ;
+        }else{
+            classificação  =  'com obesidade grau III. Cuidado!!' ;
+        }
+
+        resultado.textContent = '${nome} seu IMC é ${valorIMC} e você está ${classificacao}' ;
+
     }else{
-        resultado.textContent =  'Preencha todos os campos!!!'
+        resultado.textContent =  'Preencha todos os campos!!!' ;
     }
 }
 
