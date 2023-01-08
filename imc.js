31b025846cfe001b67ab2793ba2ecc6ed6c727e7
Carlos Moraes <113129034+Carlos-Moraes2@users.noreply.github.com>
2023-01-07 23:26:04 -0300

    correção resultado.texContent, a mensagem não aparecia conforme o esperado

--- imc.js
+++ imc.js
@@ -8,25 +8,27 @@ function imc () {
 
     if (nome !== '' && altura !== '' && peso !== '') {
 
+        
         const valorIMC = (peso / (altura * altura)).toFixed(1);
-
+         
         let classificacao = '';
 
-        if (valorIMC < 18.5){
+        if (valorIMC < 18.5 ){
             classificacao = 'abaixo do peso.';
-        }else if (valorIMC < 25) {
+            
+        }else if (valorIMC < 25 ){
             classificacao = 'com peso ideal. Parabéns!!!';
-        }else if (valorIMC <30){
+        }else if (valorIMC < 30 ){
             classificacao = 'levemente acima do peso.';
-        }else if (valorIMC <35){
+        }else if (valorIMC < 35 ){
             classificacao = 'com obesidade grau I.';
-        } else  if  ( valorIMC  <  40 ) {
-            classificação  =  'com obesidade grau II' ;
+        } else if ( valorIMC < 40 ){
+            classificacao = 'com obesidade grau II';
         }else{
-            classificação  =  'com obesidade grau III. Cuidado!!' ;
+            classificacao  =  'com obesidade grau III. Cuidado!!' ;
         }
 
-        resultado.textContent = '${nome} seu IMC é ${valorIMC} e você está ${classificacao}' ;
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
 
     }else{
         resultado.textContent =  'Preencha todos os campos!!!' ;
